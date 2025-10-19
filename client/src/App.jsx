2137ffe4c66a58a2433e5e76f324623aa2609c2f
Nada Ahmed <nada052001@gmail.com>
2025-10-19 13:04:06 +0300

    Login/Signup using clerk

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,12 +8,16 @@ import Connections from './pages/Connections';
 import Discover from './pages/Discover';
 import Profile from './pages/Profile';
 import CreatePost from './pages/CreatePost';
+import { useUser } from '@clerk/clerk-react';
+import Layout from './pages/Layout';
 
 export default function App() {
+  const user = useUser();
+  console.log(user);
   return (
     <>
       <Routes>
-        <Route path='/' element={<Login />}>
+        <Route path='/' element={!user ? <Login /> : <Layout />}>
           <Route index element={<Feed />} />
           <Route path='messages' element={<Messages />} />
           <Route path='messages/:userId' element={<ChatBox />} />
